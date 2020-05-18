3df7112de3a0f8d1741548339c35d0cceec1ef9d
synapsexdocs <synxacc@gmail.com>
2020-05-19 00:24:13 +0300

    test

--- pymdown_lexers/synapse.py
+++ pymdown_lexers/synapse.py
@@ -14,6 +14,8 @@ class SynapseLexer(RegexLexer):
 
     tokens = {
         'root': [
-            (r'.', String),
+            (r'[<>()]', Operator),
+            (r'<(.*)>', Keyword),
+            (r'>(.*)(', String),
         ]
     }
