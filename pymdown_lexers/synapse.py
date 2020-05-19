05bedf68c0eb6d7c12c17e40009860f12975590b
synapsexdocs <synxacc@gmail.com>
2020-05-19 23:07:09 +0300

    Update synapse.py

--- pymdown_lexers/synapse.py
+++ pymdown_lexers/synapse.py
@@ -16,7 +16,7 @@ class SynapseLexer(RegexLexer):
         'root': [
             (r'(<)(\w+)(>)', bygroups(token.Operator, token.Keyword, token.Operator)),
             (r'\s+', token.Text),
-            (r'(\w*?)([\),])', bygroups(token.Text, token.Operator)),
+            (r'(\w*?)(\))', bygroups(token.Text, token.Operator)),
             (r'(\w*?)(\()', bygroups(token.String, token.Operator)),
             #Edge cases and datatypes
             #(r'(<)(\w+)(,)', bygroups(token.Operator, token.Keyword, token.Operator)),
