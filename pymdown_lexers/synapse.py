83c83340b6b483fe1dce3373663e7cc13643c0a1
synapsexdocs <synxacc@gmail.com>
2020-05-20 20:20:04 +0300

    Update synapse.py

--- pymdown_lexers/synapse.py
+++ pymdown_lexers/synapse.py
@@ -20,9 +20,9 @@ class SynapseLexer(RegexLexer):
             (r'(\w*?)(\)|,)', bygroups(token.Text, token.Operator)),
             # Very bad way of doing things
             (r'([\w]*?)(\()', bygroups(token.String, token.Operator)),
-            (r'([\w]*?)(.)([\w]*?)(\()', bygroups(token.String, token.Operator, token.String)),
-            (r'([\w]*?)(.)([\w]*?)(.)([\w]*?)(\()', bygroups(token.String, token.Operator, token.String, token.Operator, token.String)),
+            (r'([\w]*?)(.)([\w]*?)(\()', bygroups(token.String, token.Operator, token.String, token.Operator)),
+            (r'([\w]*?)(.)([\w]*?)(.)([\w]*?)(\()', bygroups(token.String, token.Operator, token.String, token.Operator, token.String, token.Operator)),
             #Datatype highlighting
-            (r'(<)([\w\s,]+)(>)', bygroups(token.Operator, token.Keyword, token.Operator)),
+            (r'(<)(.+)(>)', bygroups(token.Operator, token.Keyword, token.Operator)),
         ]
     }
