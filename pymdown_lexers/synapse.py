801a4aa83220fbd9719a35298c739561b1be4de5
synapsexdocs <synxacc@gmail.com>
2020-05-20 20:32:58 +0300

    Update synapse.py

--- pymdown_lexers/synapse.py
+++ pymdown_lexers/synapse.py
@@ -15,14 +15,16 @@ class SynapseLexer(RegexLexer):
     tokens = {
         'root': [
             (r'\s+', token.Text),
-            (r',', token.Operator),
-            (r'(\w+)(:)', bygroups(token.Keyword, token.Operator)),
+            (r'[,\[\]]', token.Operator),
             (r'(\w*?)(\)|,)', bygroups(token.Text, token.Operator)),
             # Very bad way of doing things
             (r'([\w]*?)(\()', bygroups(token.String, token.Operator)),
             (r'([\w]*?)(.)([\w]*?)(\()', bygroups(token.String, token.Operator, token.String, token.Operator)),
             (r'([\w]*?)(.)([\w]*?)(.)([\w]*?)(\()', bygroups(token.String, token.Operator, token.String, token.Operator, token.String, token.Operator)),
-            #Datatype highlighting
+            # Datatype highlighting
             (r'(<)([\w\s,]+)(>)', bygroups(token.Operator, token.Keyword, token.Operator)),
+            # Edge cases
+            (r'(\w+)(:)', bygroups(token.Keyword, token.Operator)),
+            (r'(\w+)(\]\))', bygroups(token.Text, token.Operator)),
         ]
     }
