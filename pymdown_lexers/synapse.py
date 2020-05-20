7bdcf38a113b3c66b15d9d306cd841136f78e441
synapsexdocs <synxacc@gmail.com>
2020-05-20 20:53:13 +0300

    Update synapse.py

--- pymdown_lexers/synapse.py
+++ pymdown_lexers/synapse.py
@@ -16,18 +16,18 @@ class SynapseLexer(RegexLexer):
         'root': [
             (r'\s+', token.Text),
             (r'[,\[\]]', token.Operator),
-            (r'(\w*?)(\)|,)', bygroups(token.Text, token.Operator)),
+            (r'(\w+)(\)|,)', bygroups(token.Text, token.Operator)),
             # Very bad way of doing things
-            #(r'([\w]*?)(\()', bygroups(token.String, token.Operator)),
-            #(r'([\w]*?)(.)([\w]*?)(\()', bygroups(token.String, token.Operator, token.String, token.Operator)),
-            (r'([\w]*?)(.)([\w]*?)(.)([\w]*?)(\()', bygroups(token.String, token.Operator, token.String, token.Operator, token.String, token.Operator)),
-            (r'([\w]*?)(.)([\w]*?)(.)([\w]*?)(.)([\w]*?)(\()', bygroups(token.String, token.Operator, token.String, token.Operator, token.String, token.Operator, token.String, token.Operator)),
+            (r'(\w+)(\()', bygroups(token.String, token.Operator)),
+            (r'(\w+)(.)(\w+)(\()', bygroups(token.String, token.Operator, token.String, token.Operator)),
+            (r'(\w+)(.)(\w+)(.)(\w+)(\()', bygroups(token.String, token.Operator, token.String, token.Operator, token.String, token.Operator)),
+            (r'(\w+)(.)(\w+)(.)(\w+)(.)(\w+)(\()', bygroups(token.String, token.Operator, token.String, token.Operator, token.String, token.Operator, token.String, token.Operator)),
             # Datatype highlighting
             (r'(<)([\w\s\?\.,]+)(>)', bygroups(token.Operator, token.Keyword, token.Operator)),
             # Edge cases
-            #(r'(\w+)(:)', bygroups(token.Keyword, token.Operator)),
+            (r'^(\w+)(:)', bygroups(token.Keyword, token.Operator)),
             (r'(\w+)(\]\))', bygroups(token.Text, token.Operator)),
             (r'^(\w+)(.)(\w+)$', bygroups(token.Keyword, token.Operator, token.String)),
-            (r'([\w]*?)(.)([\w]*?)(.)(\*)', bygroups(token.String, token.Operator, token.String, token.Operator, token.Keyword)),
+            (r'(\w+)(.)(\w+)(.)(\*)', bygroups(token.String, token.Operator, token.String, token.Operator, token.Keyword)),
         ]
     }
